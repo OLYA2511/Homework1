27bd02ad80c84e446cbe3bb40cc3002f6576f9e0
R_ON <OLYA_FOREVER89>
2022-08-13 14:00:29 +0300

    Решение 8 задачи

---
+++ Homework1/Задача 8/Program.cs
@@ -0,0 +1,18 @@
+﻿int i = 1;
+
+Console.Write("Введите целое натуральное число n = ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+while (i <= n)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine(i);
+    }
+    i++;
+}
+
+if (n < 2)
+{
+    Console.WriteLine("Нет чётных чисел!");
+}
